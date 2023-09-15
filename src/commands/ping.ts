e961f4371883bd7efd511427408f8771b2d4ce2f
firo <appturbo102@gmail.com>
2023-09-15 07:52:49 +0000

    add embed ping commands

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,23 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { client } from "../lib/bot";
 
 async function handler(interaction: ChatInputCommandInteraction) {
-    await interaction.reply("Pong!")
+    await interaction.deferReply();
+    const reply = await interaction.fetchReply()
+    const ping = reply.createdTimestamp - interaction.createdTimestamp
+
+    await interaction.editReply({ embeds: [
+        new EmbedBuilder()
+        .setTitle('🏓 퐁!')
+        .addFields(
+            { name: '⏱ 봇 지연', value: `**${ping}**ms` },
+            { name: '⏱ API 지연', value: `**${interaction.client.ws.ping}**ms` }
+        )
+        .setTimestamp()
+    ]})
 }
 
 export default {
-    info: new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
+    info: new SlashCommandBuilder().setName("ping").setDescription("🏓 봇의 핑을 확인합니다!"),
     handler
 }
