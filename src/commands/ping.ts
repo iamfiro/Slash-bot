738db91d40f68af5c46d07c16d45c768208d0d16
firo <appturbo102@gmail.com>
2023-09-28 05:54:20 +0000

    add ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,22 @@
 import type { Command } from "octajs/dist/package/command";
-
+import { EmbedBuilder } from 'discord.js'
 const PingCommand: Command = {
   name: "ping",
   description: "You say ping, I say pong!",
   async executes(bot, interaction) {
-    interaction.reply("Pong!");
+    await interaction.deferReply();
+    const reply = await interaction.fetchReply()
+    const ping = reply.createdTimestamp - interaction.createdTimestamp
+
+    await interaction.editReply({ embeds: [
+        new EmbedBuilder()
+        .setTitle('🏓 퐁!')
+        .addFields(
+            { name: '⏱ 봇 지연', value: `\`\`\`${ping}ms\`\`\``, inline: true},
+            { name: '⏱ API 지연', value: `\`\`\`${interaction.client.ws.ping}ms\`\`\``, inline: true}
+        )
+        .setTimestamp()
+    ]})
   },
 };
 
