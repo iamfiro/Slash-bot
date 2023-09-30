47392dd498975f93af28ac8cc2e7e9d913b9d881
firo <appturbo102@gmail.com>
2023-09-30 04:17:36 +0000

    edit bot owner updateAlarm.ts

--- src/commands/manage/updateAlarm.ts
+++ src/commands/manage/updateAlarm.ts
@@ -12,7 +12,7 @@ const command: Command = {
     },
   },
   async executes(bot, interaction) {
-    if(interaction.user.id === '535676248513314816') return await interaction.reply({ content: '❌ 권한이 없습니다', ephemeral: true})
+    if(interaction.user.id !== '535676248513314816') return await interaction.reply({ content: '❌ 권한이 없습니다', ephemeral: true})
     await interaction.reply({ content: '✅ 알림 전송됨', ephemeral: true})
     const version = interaction.options.getString('version') || '';
     (bot.channels.cache.get("1156160717087572008") as TextChannel).send(`✅ 봇이 업데이트 되었습니다 - **${version}**\n자세한 패치노트는 <id:customize>에서 \`츠니봇 알림 받기\`를 눌러주세요`);
